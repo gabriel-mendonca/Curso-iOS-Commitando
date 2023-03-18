898a5e109a20a09fdecbfc4c5f59549b2bef9f68
Gabriel <gabrielcruzerence9993@gmail.com>
2023-03-18 20:05:49 -0300

    Initial Commit

---
+++ RestaurantDomain/RemoteRestaurantLoader.swift
@@ -0,0 +1,43 @@
+//
+//  RemoteRestaurantLoader.swift
+//  RestaurantDomain
+//
+//  Created by Gabriel Estudos on 18/03/23.
+//
+
+import Foundation
+
+enum NetworkState {
+    case success
+    case error(Error)
+}
+
+protocol NetworkClient {
+    func request(from url: URL, completion: @escaping (NetworkState) -> Void)
+}
+
+final class RemoteRestaurantLoader {
+    let url: URL
+    let networkClient: NetworkClient
+    
+    enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    init(url: URL, networkClient: NetworkClient) {
+        self.url = url
+        self.networkClient = networkClient
+    }
+    
+    func load(completion: @escaping (RemoteRestaurantLoader.Error) -> Void) {
+        networkClient.request(from: url) { state in
+            switch state {
+            case .success:
+                completion(.invalidData)
+            case .error:
+                completion(.connectivity)
+            }
+        }
+    }
+}
